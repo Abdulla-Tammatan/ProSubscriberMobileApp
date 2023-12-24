70f101cc6b7d465992d64c315ee7c78fc9f541ba
mxnxl1506 <manalharoon9@gmail.com>
2023-12-24 19:15:58 +0400

    Testing

--- app/src/main/java/com/example/prosubscriberapp/service/UserService.kt
+++ app/src/main/java/com/example/prosubscriberapp/service/UserService.kt
@@ -22,7 +22,7 @@ class UserService {
                 // Add other user properties as needed
             }.value
 
-            // Now, update the user with additional details
+            //Update the user with additional details
             updateUserDetails(userId, user)
         }
     }
